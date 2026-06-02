1c0a7a80fdc93cd705c8962f487dcb18d0407ae7
akshayy018 <akshaychauhan21122008@gmail.com>
2026-06-02 19:21:12 +0530

    Day 2 uploaded

---
+++ Palindrome.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, original, rev = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0)
+    {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+
+    if (original == rev)
+        printf("Palindrome Number");
+    else
+        printf("Not a Palindrome Number");
+
+    return 0;
+}
+
+
+
+
+
